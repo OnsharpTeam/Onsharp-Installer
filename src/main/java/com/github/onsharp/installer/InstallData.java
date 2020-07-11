ebbf053acea3d9023f6a6e1674e22aec266fe0a7
DasDarki <rageskilln@gmail.com>
2020-07-11 19:58:42 +0200

    adding installer

---
+++ src/main/java/com/github/onsharp/installer/InstallData.java
@@ -0,0 +1,11 @@
+package com.github.onsharp.installer;
+
+import lombok.Data;
+
+@Data
+public class InstallData {
+
+    private String version;
+    private String timestamp;
+    private String file;
+}
---
+++ src/main/java/com/github/onsharp/installer/OnsharpInstaller.java
@@ -0,0 +1,82 @@
+package com.github.onsharp.installer;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class OnsharpInstaller {
+
+    private static final String INSTALL_URL = "https://eternitylife.de/onsharp_update.json";
+    private static final Gson GSON = new Gson();
+
+    public static void main(String[] args) throws Exception {
+        String destPath = new File(OnsharpInstaller.class.getProtectionDomain().getCodeSource().getLocation()
+                .toURI()).getAbsolutePath();
+        String configPath = destPath + "/server_config.json";
+        if (!new File(configPath).exists()) {
+            System.out.println("[Onsharp-Installer] NO SERVER CONFIG FILE FOUND: THE INSTALLER MUST BE IN THE SERVER DIRECTORY!");
+            return;
+        }
+        System.out.println("[Onsharp-Installer] Retrieving dist meta...");
+        InstallData data = getData();
+        System.out.println("[Onsharp-Installer] Download install file...");
+        Path filePath = Paths.get(destPath, "install.zip");
+        downloadFile(data.getFile(), filePath);
+        System.out.println("[Onsharp-Installer] Installing files...");
+        Unzipper.unzip(filePath, Paths.get(destPath));
+        System.out.println("[Onsharp-Installer] Adjusting server...");
+        JsonParser parser = new JsonParser();
+        JsonObject config = parser.parse(new String(Files.readAllBytes(Paths.get(configPath)))).getAsJsonObject();
+        JsonArray plugins = config.getAsJsonArray("plugins");
+        if (contains(plugins, "onsharp-runtime"))
+            plugins.add("onsharp-runtime");
+        JsonArray packages = config.getAsJsonArray("packages");
+        if (contains(packages, "onsharp"))
+            packages.add("onsharp");
+        String json = new GsonBuilder().setPrettyPrinting().create().toJson(config);
+        Files.write(Paths.get(configPath), json.getBytes());
+        System.out.println("[Onsharp-Installer] Cleaning environment...");
+        filePath.toFile().delete();
+        System.out.println("[Onsharp-Installer] Successfully installed!");
+    }
+
+    private static boolean contains(JsonArray array, String item) {
+        for (int i = 0; i < array.size(); i++) {
+            if (array.get(i).getAsString().equals(item)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static InstallData getData() throws Exception {
+        URL url = new URL(INSTALL_URL);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+        StringBuilder json = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null)
+            json.append(line);
+        reader.close();
+        return GSON.fromJson(json.toString(), InstallData.class);
+    }
+
+    private static void downloadFile(String fileUrl, Path file) throws Exception {
+        URL url = new URL(fileUrl);
+        try (InputStream in = url.openStream()) {
+            Files.copy(in, file, StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+}
